cbb5777b4de0a39050284d5300e898d654bfe374
Dmitry Bobylev <dimas.od@gmail.com>
2016-07-04 23:11:35 +0300

    Highlighter for everyone who plays overwatch

---
+++ highlighter/__init__.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+from plugin import Plugin
+from utils import is_text_message
+
+
+class Highlighter(Plugin):
+
+    async def on_chat_message(self, message):
+        if not is_text_message(message):
+            return
+
+        try:
+            message_text = message['text']
+        except KeyError:
+            return
+        for trigger in self.settings.get('triggers'):
+            if trigger in message_text.lower():
+                words = []
+                words.append(self.settings.get('players'))
+                return "ОГОНЬ ПО ГОТОВНОСТИ! " + " ".join(words)
