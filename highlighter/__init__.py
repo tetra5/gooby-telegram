eb6aea34e785bfdf75192680dd433c701937c81c
Dmitry Bobylev <dimas.od@gmail.com>
2016-07-04 23:17:14 +0300

    Update __init__.py
    fixed

--- highlighter/__init__.py
+++ highlighter/__init__.py
@@ -18,6 +18,4 @@ class Highlighter(Plugin):
             return
         for trigger in self.settings.get('triggers'):
             if trigger in message_text.lower():
-                words = []
-                words.append(self.settings.get('players'))
-                return "ОГОНЬ ПО ГОТОВНОСТИ! " + " ".join(words)
+                return "ОГОНЬ ПО ГОТОВНОСТИ! " + self.settings.get('players')
